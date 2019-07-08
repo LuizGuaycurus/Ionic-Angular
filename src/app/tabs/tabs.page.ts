eb48b7a1e586b6b46129b2082b26a0a7b49fe938
fmoreirati <fabianomoreira.ti@gmail.com>
2019-07-08 12:02:39 -0300

    08072019

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UsuarioService } from './../services/usuario.service';
 
 @Component({
   selector: 'app-tabs',
@@ -7,6 +8,17 @@ import { Component } from '@angular/core';
 })
 export class TabsPage {
 
-  constructor() {}
+  protected nuser: number = 0;
+
+  constructor(
+    private usuarioService: UsuarioService
+  ) {
+    this.usuarioService.getAll()
+      .subscribe(
+        res => this.nuser = res.length
+        ,
+        err => this.nuser = 0
+      )
+  }
 
 }
